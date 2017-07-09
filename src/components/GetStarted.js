25958660558a6c9e5d3c73d1a08402bde61f84a2
guipasmoi <guipasmoi@hotmail.com>
2017-07-09 22:46:05 +0200

    fix: mistakes

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 export default class GetStarted extends Component {
     render() {
         return (
-
           <p className="App-intro">
               To get started, edit <code>src/App.js</code> and save to reload.
           </p>
